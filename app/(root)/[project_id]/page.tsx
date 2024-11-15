1f86eeb93ad63639ac80015ce2aee9700e2de842
gauravdubey19 <dubeygaurav520@gmail.com>
2024-11-15 18:56:59 +0530

    upd - meetings

--- app/(root)/[project_id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-
-export default async function ProjectPage() {
-  const session = await getServerSession();
-  if (!session) redirect("/auth/sign-in");
-  redirect("/project-id/dashboard");
-  return <div>AdminPage</div>;
-}
