3648db8cbfbbbc38c5c310eb8884055c6e7c80e6
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-12-03 17:40:39 +0530

    Refactor Add chat page

--- app/(root)/c/project/[name]/chats/components/addChat.tsx
+++ app/(root)/c/project/[name]/chats/components/addChat.tsx
@@ -1,39 +1,36 @@
-'use client';
+"use client";
 import SquareButton from "@/components/reusables/wrapper/squareButton";
+import { DialogClose } from "@/components/ui/dialog";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Search } from "lucide-react";
 
-export default function AddChat({
-  setIsAddChatOpen,
-}: {
-  setIsAddChatOpen: (value: boolean) => void;
-}) {
+export default function AddChat() {
   return (
+    // <div
+    //   onClick={() => setIsAddChatOpen(false)}
+    //   className="z-5 absolute flex items-center justify-center  right-0 bottom-0 left-0 h-[calc(100vh-70px)] w-full bg-black/30"
+    // >
     <div
-      onClick={() => setIsAddChatOpen(false)}
-      className="z-5 absolute flex items-center justify-center  right-0 bottom-0 left-0 h-[calc(100vh-70px)] w-full bg-black/30"
+      onClick={(e) => e.stopPropagation()}
+      className="z-10 w-[733px] m-4  bg-background flex flex-col gap-6 rounded-3xl   p-5 lg:p-6 "
     >
-      <div
-        onClick={(e) => e.stopPropagation()}
-        className="z-10 w-[733px] m-4  bg-background flex flex-col gap-6 rounded-3xl   p-5 lg:p-6 "
-      >
-        <h1 className="text-2xl font-semibold text-dark-gray">Add A Chat</h1>
-        <div className="flex flex-col gap-3 w-full">
-          <span className="relative w-full ">
-            <input
-              placeholder="Search by name or role"
-              className=" bg-[#E4EEFE] w-full  py-3 px-6 pl-11 outline-none shadow_search border-[#C1C7CD] rounded-xl placeholder:text-[#697077] placeholder:font-medium placeholder:text-base  "
-              type="search"
-              name=""
-              id=""
-            />
-            <Search
-              color="#697077"
-              strokeWidth={2.6}
-              className="absolute h-[20px] w-[20px] top-[14px] left-4 font-medium"
-            />
-          </span>
-          <ScrollArea className=" h-[40vh]">
+      <h1 className="text-2xl font-semibold text-dark-gray">Add A Chat</h1>
+      <div className="flex flex-col gap-3 w-full">
+        <span className="relative w-full ">
+          <input
+            placeholder="Search by name or role"
+            className=" bg-[#E4EEFE] w-full  py-3 px-6 pl-11 outline-none shadow_search border-[#C1C7CD] rounded-xl placeholder:text-[#697077] placeholder:font-medium placeholder:text-base  "
+            type="search"
+            name=""
+            id=""
+          />
+          <Search
+            color="#697077"
+            strokeWidth={2.6}
+            className="absolute h-[20px] w-[20px] top-[14px] left-4 font-medium"
+          />
+        </span>
+        <ScrollArea className=" h-[40vh]">
           <div className="flex flex-col p-3 ">
             <Card />
             <Card />
@@ -42,14 +39,35 @@ export default function AddChat({
             <Card />
             <Card />
             <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
           </div>
-          </ScrollArea>
-        </div>
-        <SquareButton className="text-[#6A6A6A] w-fit self-end" onClick={() => setIsAddChatOpen(false)}>
+        </ScrollArea>
+      </div>
+      <DialogClose asChild>
+        <SquareButton
+          className="text-[#6A6A6A] w-fit self-end"
+        >
           Cancel
         </SquareButton>
-      </div>
+      </DialogClose>
     </div>
+    // </div>
   );
 }
 
--- app/(root)/c/project/[name]/chats/components/headline.tsx
+++ app/(root)/c/project/[name]/chats/components/headline.tsx
@@ -3,26 +3,31 @@ import WrHeadline from "@/components/reusables/wrapper/Headline";
 import SquareButton from "@/components/reusables/wrapper/squareButton";
 import { Role, VENDOR } from "@/types";
 import { Plus } from "lucide-react";
-export default function Headline({
-  role,
-  setIsAddChatOpen,
-}: {
-  role: Role;
-  setIsAddChatOpen: (value: boolean) => void;
-}) {
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import AddChat from "./addChat";
+export default function Headline({role}:{role:Role}) {
   return (
     <WrHeadline title="Chats">
-      <SquareButton
+      
+      <Dialog>
+        <DialogTrigger>
+        <SquareButton
         role={role}
-        onClick={() => {
-          setIsAddChatOpen(true);
-        }}
       >
         <Plus
           color={role === VENDOR ? "var(--vendor-dark)" : "var(--primary-blue)"}
         />
         Add Chat
       </SquareButton>
+        </DialogTrigger>
+        <DialogContent>
+          <AddChat/>
+        </DialogContent>
+      </Dialog>
     </WrHeadline>
   );
 }
