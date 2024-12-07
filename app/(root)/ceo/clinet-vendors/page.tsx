2b2d94fa53e255f4f899389f98da37bfea8d2b98
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-12-07 12:22:12 +0530

    new ui components

---
+++ app/(root)/ceo/clinet-vendors/page.tsx
@@ -0,0 +1,8 @@
+import ClinetProfile from '@/components/pages/client-vendors/clientProfile/ceo/page'
+import React from 'react'
+
+export default function Page() {
+  return (
+    <ClinetProfile/>
+  )
+}
---
+++ app/(root)/ceo/profile/page.tsx
@@ -0,0 +1,9 @@
+import Profile from "@/components/pages/profile/ceo/page";
+
+
+export default function Page() {
+
+  return (
+      <Profile />
+  );
+}
