0a13c291792bdef1b71830c02ed81a39412dba5a
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-12-14 19:00:47 +0530

    Update loader

--- components/ui/Loader.tsx
+++ components/ui/Loader.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function Loader({
-  className = "h-[calc(100vh-60px)]",
+  className = "",
   text,
 }: {
   className?: string;
@@ -10,7 +10,7 @@ export default function Loader({
   return (
     <>
       <div
-        className={`mt-[60px] w-full ${className} flex-center flex-col gap-4 bg-transparent animate-pulse`}
+        className={`w-full   flex-1 p-3 sm:p-4 sm:px-6  flex flex-col  overflow-auto bg-primary-sky-blue  ${className} flex-center flex-col gap-4 animate-pulse`}
       >
         <Image
           src="/logo.png"
